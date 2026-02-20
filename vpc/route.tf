785c9d6aa4f374dd282c8b5ed4bd5d8482e6c432
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-20 08:39:33 +0500

    Add route table to route  internet traffic between Internet Gateway with the Public Subnet

---
+++ vpc/route.tf
@@ -0,0 +1,18 @@
+resource "aws_route_table" "route_table" {
+  vpc_id = aws_vpc.vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Project = "${local.project_name}"
+  }
+
+}
+
+resource "aws_route_table_association" "route_table_association" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.route_table.id
+}
