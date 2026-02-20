df163adfa1d65c95220beb53ec5f3026604bc043
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-20 09:46:01 +0500

    Enable access logs, provide multiple AZs for the sake of HA. Configure corss region load balancer. Add tag.

--- ec2/lb.tf
+++ ec2/lb.tf
@@ -2,6 +2,13 @@ resource "aws_elb" "nginx_lb" {
   name            = "nginx-classic-lb"
   subnets         = [data.terraform_remote_state.network.outputs.public_subnet_id]
   security_groups = [data.terraform_remote_state.network.outputs.lb_sg_id]
+  availability_zones = ["eu-south-1a", "eu-south-1b"]
+  
+  access_logs {
+    bucket        = "epam-logs" // This bucket has globally unique name :) 
+    bucket_prefix = "epam-terraform-logs"
+    interval      = 60
+  }
 
   listener {
     instance_port     = 80
@@ -19,4 +26,12 @@ resource "aws_elb" "nginx_lb" {
   }
 
   instances = aws_instance.nginx[*].id
+  cross_zone_load_balancing   = true
+  idle_timeout                = 400
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "terraform-elb"
+  }
 }
