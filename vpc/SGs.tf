3cf85d9ed327dc2aa8ca5c253644df0c2b6cf40f
Yerdaulet <yerdaulet.zhumabay@gmail.com>
2026-02-20 08:45:33 +0500

    Add security group for Classical load balancer and nginx instances

---
+++ vpc/SGs.tf
@@ -0,0 +1,41 @@
+# Security Group for the Public Classic Load Balancer
+resource "aws_security_group" "lb_sg" {
+  name        = "classic-lb-sg"
+  description = "Allow HTTP traffic from the internet"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# Security Group for Nginx Instances (Not internet facing)
+resource "aws_security_group" "nginx_sg" {
+  name        = "nginx-instance-sg"
+  description = "Allow traffic ONLY from the Load Balancer"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    from_port       = 80
+    to_port         = 80
+    protocol        = "tcp"
+    security_groups = [aws_security_group.lb_sg.id]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
